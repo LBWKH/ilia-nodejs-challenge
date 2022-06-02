df02595b410c0f0519aab896e9140b09ab5e92ae
leonardbwkh <leonard_bwkh@hotmail.com>
2022-06-02 19:38:36 -0300

    transactions table migration in db

---
+++ wallet/prisma/migrations/20220602223351_transactions_table_creation/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('CREDIT', 'DEBIT');
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
